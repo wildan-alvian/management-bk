5c5f94da050e1a81f88938271035a1e892cbd912
Agus Adyandana <aadyandana@gmail.com>
2025-05-15 21:35:35 +0700

    Feat (notification): Add notification badge feature

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Notification;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        view()->composer('layout.index', function($view) {
+            $user = Auth::user();
+
+            $notifications = Notification::orderBy('created_at', 'desc');
+            if ($user->hasRole(['Guidance Counselor', 'Student', 'Student Parents'])) {
+                if ($user->hasRole(['Student', 'Student Parents'])) {
+                    $notifications = $notifications->where('user_id', $user->id);
+                }
+                $notifications = $notifications->where('type', $user->role);
+            }
+
+            $unreadCount = $notifications->where('status', false)->count();
+
+            $view->with('unreadCountBadge', $unreadCount);
+        });
     }
 }

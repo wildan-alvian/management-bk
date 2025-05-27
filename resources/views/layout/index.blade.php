b304f161bcdd83a607885c224b4f4e19264c1d25
Zazan Pratama <mail@zazan.me>
2025-05-27 13:37:23 +0700

    fix mobile menu

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -667,6 +667,7 @@
                 const menuBtn = document.createElement('button');
                 menuBtn.className = 'btn btn-primary position-fixed top-0 start-0 m-3 d-md-none';
                 menuBtn.innerHTML = '<i class="bi bi-list"></i>';
+                menuBtn.style.zIndex = "1100";
                 menuBtn.onclick = () => document.querySelector('.sidebar').classList.toggle('show');
                 document.body.appendChild(menuBtn);
             }
